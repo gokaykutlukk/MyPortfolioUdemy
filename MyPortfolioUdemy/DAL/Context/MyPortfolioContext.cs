5a08eccec175f5bb69a6023e22040f0ef4ebc687
Gökay KUTLUK <gokaykutluk1@gmail.com>
2025-04-03 20:26:46 +0300

    Add project files.

---
+++ MyPortfolioUdemy/DAL/Context/MyPortfolioContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyPortfolioUdemy.DAL.Context
+{
+    public class MyPortfolioContext: DbContext //Db context sınıfına ait sınıflar var onları kullanabilirsin overide= mevcuttak metdou benim istediğim gibi kulllan
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=GOKAY\\SQLEXPRESS;initial Catalog=MyPortfolioDb;integrated Security=true;");
+        }
+
+        public DbSet<Entities.SocialMedia> SocialMedias { get; set; }
+        public DbSet<Entities.About> Abouts { get; set; }
+        public DbSet<Entities.Skill> Skills { get; set; }
+        public DbSet<Entities.Experience> Experiences { get; set; }
+        public DbSet<Entities.Portfolio> Portfolios { get; set; }
+        public DbSet<Entities.Message> Messages { get; set; }
+        public DbSet<Entities.Contact> Contacts { get; set; }
+        public DbSet<Entities.Testimonial> Testimonials { get; set; }
+        public DbSet<Entities.Feature> Features{ get; set; } 
+    }
+}
